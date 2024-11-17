87487088155514e31aabff5c2dcd86422bb338bd
Jhordy272 <jaagatonp@udistrital.edu.co>
2024-11-17 10:38:29 -0500

    Add the method totalCost, testTotalCost, word, testWord

--- main.py
+++ main.py
@@ -7,8 +7,32 @@ def testDictionary():
     print(d.look("Apple"))
     print(d.look("Banana"))
 
+def totalCost(items,tax):
+    costs = {'socks': 5, 'shoes': 60, 'sweater': 30}
+    total = 0
+    for item in items:
+        if item in costs:
+            total += costs[item]
+    return total+total*tax
+
+def testTotalCost():
+    print("*** Testing totalCost")
+    print(totalCost(['socks','shoes'],0.09))
+
+def word(words):
+    newWord = ""
+    for i,word in enumerate(words):
+        newWord += word[i]
+    return newWord
+
+def testWord():
+    print("*** Testing word")
+    print(word(["yoda", "best","has"]))
+
 def __main__():
     testDictionary()
+    testTotalCost()
+    testWord()
 
 if __name__ == "__main__":
     __main__()
